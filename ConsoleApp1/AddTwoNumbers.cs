aa8cabd7d300d7d8bfa171da08d7c61f9d427be0
kenlolmen <ojjaruga@gmail.com>
2024-12-07 15:00:04 +0100

    Add new files

---
+++ ConsoleApp1/AddTwoNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public class ListNode 
+    {
+        public int val;
+        public ListNode next;
+
+    public ListNode(int val=0, ListNode next=null) 
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class AddTwoNumbers
+    {
+        public ListNode Add(ListNode l1, ListNode l2)
+        {
+            return 
+        }
+    }
+}
---
+++ ConsoleApp1/TwoSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TwoSum
+    {
+        public int[] Function(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+
+                }
+            }
+            throw new ArgumentException("No solution found");
+        }
+
+        public void implementation()
+        {
+            int[] n1 = { 2, 7, 11, 15 };
+            int t1 = 9;
+
+            int[] n2 = { 3, 2, 4 };
+            int t2 = 6;
+
+            int[] n3 = { 3, 3 };
+            int t3 = 6;
+
+            Console.WriteLine($"[{string.Join(", ", Function(n1, t1))}]");
+            Console.WriteLine($"[{string.Join(", ", Function(n2, t2))}]");
+            Console.WriteLine($"[{string.Join(", ", Function(n3, t3))}]");
+        }
+    }
+}
